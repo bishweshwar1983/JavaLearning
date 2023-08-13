5a01f178ecae614a868e2699a207a06e26d9abde
bishweshwar1983 <50376836+bishweshwar1983@users.noreply.github.com>
2023-08-13 11:33:38 +0100

    created new project

---
+++ Algorithms/src/Main.java
@@ -0,0 +1,73 @@
+import java.lang.Math;
+import java.util.*;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+        int fac;
+
+        Exercise1 exe1 = new Exercise1();
+        // exe1.reverse();
+        // System.out.println(exe1.calcFalc(4));
+        // System.out.println(exe1.calc_avg());
+        // exe1.find_common();
+        // System.out.println(exe1.conc_str());
+     /*   if (exe1.check_prime(1)) {
+            System.out.println("prime number");
+        }
+        else {
+            System.out.println("Not prime");
+        }
+        */
+        exe1.gcd(24,12);
+
+
+/*
+
+    Algo1 myobj = new Algo1();
+    myobj.Largestnumber();
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10);
+            System.out.println(array[i]);
+        }
+
+        ArrayList<Integer> newarraylist = new ArrayList<>();
+
+
+        for (int i = 0; i < 5; i++) {
+            newarraylist.add((int) (Math.random() * 10));
+        }
+
+        for (int e : newarraylist) {
+            System.out.println(e);
+            System.out.println(newarraylist.size());
+        }
+
+        Stack<Integer> newstack = new Stack<>();
+        newstack.push(3);
+        newstack.push(2);
+        System.out.println(newstack.pop());
+        System.out.println(newstack.pop());
+
+
+        Queue<Integer> newqueue = new LinkedList<>();
+        newqueue.add(10);
+        newqueue.add(20);
+
+        System.out.println(newqueue.remove());
+        System.out.println(newqueue.remove());
+
+
+        Map<String, Integer> newmap = new HashMap<>();
+        newmap.put("one", 1);
+        newmap.put("two", 2);
+
+        System.out.println(newmap.get("one"));
+*/
+
+    }
+}
