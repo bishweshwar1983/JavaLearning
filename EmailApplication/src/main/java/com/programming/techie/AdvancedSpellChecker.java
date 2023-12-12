7de29a739734c49bcc00e6df09ee2195694bc7a5
Bishweshwar Pradhan <bish@Bishweshwars-MacBook-Air.local>
2023-12-12 17:29:10 +0000

    new changes

--- EmailApplication/src/main/java/com/programming/techie/AdvancedSpellChecker.java
+++ EmailApplication/src/main/java/com/programming/techie/AdvancedSpellChecker.java
@@ -4,7 +4,7 @@ public class AdvancedSpellChecker implements SpellChecker{
 
     public void checkSpelling(String emailMessage){
         if (emailMessage!= null){
-            System.out.println("Spell Checking Completed.....");
+            System.out.println("Spell Checking Completed....using Advanced.");
         } else {
             throw new RuntimeException("An exception has occurred....");
         }
--- EmailApplication/src/main/java/com/programming/techie/BasicSpellChecker.java
+++ EmailApplication/src/main/java/com/programming/techie/BasicSpellChecker.java
@@ -3,7 +3,7 @@ package com.programming.techie;
 public class BasicSpellChecker implements SpellChecker{
     public void checkSpelling(String emailMessage){
         if(emailMessage != null){
-            System.out.println("Checking spelling.....");
+            System.out.println("Checking spelling.....using BasicSpell");
             System.out.println("Spell Checking completed.....");
         } else {
             throw new RuntimeException("An Exception has occurred....");
