78af07343658b894d92ef212aa7543d577fa9d83
Bishweshwar Pradhan <bish@Bishweshwars-MacBook-Air.local>
2023-12-12 16:42:35 +0000

    new changes

--- RestService/src/main/java/com/example/demo/DemoApplication.java
+++ RestService/src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,17 @@
 package com.example.demo;
-
+import org.springframework.core.SpringVersion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class DemoApplication {
 	public static void main(String[] args) {
+//		System.out.println("version: " + SpringVersion.getVersion());
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+
 
 }
